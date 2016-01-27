e69223dad4e629f41ae6c1fce547221d2431909c
Alex Kalinin <login.hedin@gmail.com>
2016-01-27 21:43:05 +0600

    + pass file through rails; + receive file with 1C

--- rails/app/controllers/application_controller.rb
+++ rails/app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def get_file
+    puts request.inspect
+    response.headers['Guid'] = SecureRandom.uuid
+    send_file Rails.root.join('app', 'assets', 'images', 'testpicture.jpeg')
+  end
 end
--- rails/config/application.rb
+++ rails/config/application.rb
@@ -22,5 +22,7 @@ module Soapws
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.middleware.use Rack::ContentLength
   end
 end
